6977dabb90eac85f87d07fc467c8d15ad6e55ec0
Md Mirza Shihab <msmd2@graduate.utm.my>
2024-05-28 15:55:39 +0600

    Classname Corrected  from Class

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -75,11 +75,11 @@ function Sample() {
                   id="inline-checkbox"
                   type="checkbox"
                   value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                 />
                 <label
                   for="inline-checkbox"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="ms-2 text-sm font-medium text-gray-900"
                 >
                   Amar Lab
                 </label>
@@ -89,11 +89,11 @@ function Sample() {
                   id="inline-2-checkbox"
                   type="checkbox"
                   value=""
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
                 />
                 <label
                   for="inline-2-checkbox"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="ms-2 text-sm font-medium text-gray-900"
                 >
                   Arogga
                 </label>
