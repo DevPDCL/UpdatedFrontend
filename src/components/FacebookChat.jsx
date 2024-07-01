8b9b8a0a6c9ea666c5dabe8d3c0f202a9030fc39
Sakib0017 <sakibkamal0017@gmail.com>
2024-07-01 19:45:10 +0600

    facebook messanger added

--- src/components/FacebookChat.jsx
+++ src/components/FacebookChat.jsx
@@ -3,15 +3,23 @@ import React, { useEffect } from 'react';
 const FacebookChat = () => {
     useEffect(() => {
         const fbCustomerChat = document.createElement('div');
-        fbCustomerChat.className = 'fb-customerchat fixed bottom-10 right-10 bg-black h-20 w-20';
+        fbCustomerChat.className = 'fb-customerchat';
         fbCustomerChat.setAttribute('attribution', 'setup_tool');
-        fbCustomerChat.setAttribute('page_id', '196298460417225');
+        fbCustomerChat.setAttribute('page_id', '196298460417225'); // Replace with your Facebook Page ID
         document.body.appendChild(fbCustomerChat);
+        console.log("Facebook Chat Div Created", fbCustomerChat);
 
-        if (window.FB) {
-            window.FB.XFBML.parse();
-            console.log("FB Chat Plugin Parsed");
-        }
+        const checkFbInit = () => {
+            if (window.FB) {
+                window.FB.XFBML.parse();
+                console.log("FB Chat Plugin Parsed");
+            } else {
+                console.log("FB SDK not ready, retrying...");
+                setTimeout(checkFbInit, 500);
+            }
+        };
+
+        checkFbInit();
 
         return () => {
             document.body.removeChild(fbCustomerChat);
