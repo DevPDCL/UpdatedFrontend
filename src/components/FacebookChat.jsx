d9b5153c0174aab3163c7edf02bc1b343f9adcd9
Sakib0017 <sakibkamal0017@gmail.com>
2024-07-02 10:47:26 +0600

    Whatsapp & Messanger Re-placed

--- src/components/FacebookChat.jsx
+++ src/components/FacebookChat.jsx
@@ -1,32 +1,16 @@
-import React, { useEffect } from 'react';
+"use client";
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
 
 const FacebookChat = () => {
-    useEffect(() => {
-        const fbCustomerChat = document.createElement('div');
-        fbCustomerChat.className = 'fb-customerchat';
-        fbCustomerChat.setAttribute('attribution', 'setup_tool');
-        fbCustomerChat.setAttribute('page_id', '196298460417225'); // Replace with your Facebook Page ID
-        document.body.appendChild(fbCustomerChat);
-        console.log("Facebook Chat Div Created", fbCustomerChat);
-
-        const checkFbInit = () => {
-            if (window.FB) {
-                window.FB.XFBML.parse();
-                console.log("FB Chat Plugin Parsed");
-            } else {
-                console.log("FB SDK not ready, retrying...");
-                setTimeout(checkFbInit, 500);
-            }
-        };
-
-        checkFbInit();
-
-        return () => {
-            document.body.removeChild(fbCustomerChat);
-        };
-    }, []);
-
-    return null;
+    
+    return (
+        <>
+            <FacebookProvider appId="2480202145700902" chatSupport>
+                <CustomChat pageId="196298460417225" minimized={true} />
+            </FacebookProvider>  
+        </>
+    );
 };
 
 export default FacebookChat;
