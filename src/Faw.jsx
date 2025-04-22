5ba431351eeda0eaafb6f21b6c6493624882ead1
Md Mirza Shihab <msmd2@graduate.utm.my>
2025-04-22 18:56:02 +0600

    Kept backup of doctor search component in faw component.

--- src/Faw.jsx
+++ src/Faw.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "@fontsource/ubuntu";
-import axios from "axios"; // Import Axios
-import { Details } from "../components";
 import { motion } from "framer-motion";
-import { branch } from "../constants";
+import { doctorData1 } from "../constants";
+import { Link } from "react-router-dom";
 
 const spring = {
   type: "spring",
   stiffness: 700,
   damping: 30,
 };
+
 const buttonVariants = {
   initial: { opacity: 1, scale: 1 },
   animate: { opacity: 1, scale: 1.1 },
@@ -21,217 +21,253 @@ const searchBoxVariants = {
   hover: { scale: 1.03 },
 };
 
-const BranchContact = ({ address, Hotline, Email }) => {
+const DoctorCard = ({ doctor }) => {
+  const cardBackgroundColor =
+    doctor.drGender === "Female"
+      ? "bg-gradient-to-b from-transparent via-transparent to-[#fce8f3]/80"
+      : "bg-gradient-to-b from-transparent via-transparent to-[#f0fff0]/80";
+
+  const backgroundColor =
+    doctor.drGender === "Female"
+      ? "group-hover:bg-[#fce8f3]"
+      : "group-hover:bg-[#d7ffd7]";
+
+  const textColor =
+    doctor.drGender === "Female" ? "text-[#5E2750]" : "text-[#00984a]";
+
   return (
-    <div className="h-[180px] flex flex-col justify-between text-gray-500 p-2 font-ubuntu text-[16px]">
-      <p>
-        <span className="text-[18px] font-medium font-ubuntu py-2">
-          Address:{" "}
-        </span>
-        {address}
-      </p>
-      <p>
-        <span className="text-[18px] font-medium font-ubuntu py-1">
-          Hotline:{" "}
-        </span>
-        {Hotline}
-      </p>
-      <p>
-        <span className="text-[18px] font-medium font-ubuntu py-1">
-          Email:{" "}
-        </span>
-        {Email}
-      </p>
-    </div>
+    <Link
+      to={`/doctordetail/${doctor.drID}`}
+      className="doctor-card-link group">
+      {/* Wrap the entire DoctorCard with the Link */}
+      <div
+        className={`relative flex w-72 flex-col rounded-xl ${cardBackgroundColor} bg-clip-border text-gray-700 shadow-md h-full transition-transform duration-700 transform hover:-translate-y-3`}>
+        <div className="relative mx-4 mt-4 h-100 overflow-hidden rounded-xl bg-clip-border text-gray-700 shadow-lg">
+          {doctor.image ? (
+            <img
+              src={doctor.image}
+              alt={`${doctor.drName}'s picture`}
+              className={`w-full shadow-xl ${backgroundColor} rounded-3xl object-cover`}
+            />
+          ) : (
+            <div className="no-image font-ubuntu flex flex-col justify-center items-center p-2 h-60">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="256"
+                height="256"
+                viewBox="0 0 256 256">
+                <g transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
+                  <circle cx="58.145" cy="74.615" r="13.145" fill="#ffffff" />
+                  <path
+                    d="M45 40.375c-9.415 0-17.118-7.703-17.118-17.118v-6.139C27.882 7.703 35.585 0 45 0s17.118 7.703 17.118 17.118v6.139C62.118 32.672 54.415 40.375 45 40.375z"
+                    fill="#d7ffd7"
+                  />
+                  <path
+                    d="M55.078 42.803L45 54.44 34.922 42.803c-12.728 2.118-22.513 13.239-22.513 26.544v17.707c0 1.621 1.326 2.946 2.946 2.946h59.29c1.621 0 2.946-1.326 2.946-2.946V69.346c0-13.305-9.786-24.426-22.513-26.544zM67.204 76.875c0 .667-.541 1.208-1.208 1.208h-3.877v3.877c0 .667-.541 1.208-1.208 1.208H56.73c-.667 0-1.208-.541-1.208-1.208v-3.877h-3.877c-.667 0-1.208-.541-1.208-1.208v-4.179c0-.667.541-1.208 1.208-1.208h3.877V67.61c0-.667.541-1.208 1.208-1.208h4.179c.667 0 1.208.541 1.208 1.208v3.877h3.877c.667 0 1.208.541 1.208 1.208v4.179z"
+                    fill="#d7ffd7"
+                  />
+                </g>
+              </svg>
+              <p>No Image Available</p>
+            </div>
+          )}
+        </div>
+        <div className="p-6 text-center">
+          <h4
+            className={`mb-2 block font-sans ${textColor} text-2xl font-semibold leading-snug tracking-normal antialiased`}>
+            {doctor.drName}
+          </h4>
+          <p className="block bg-gradient-to-tr from-gray-400 to-gray-600 bg-clip-text font-sans text-base font-medium leading-relaxed text-transparent antialiased">
+            <strong>Degrees:</strong> {doctor.drDegree}
+          </p>
+        </div>
+        <div className="flex justify-center gap-7 p-6 pt-2"></div>
+      </div>
+    </Link>
   );
 };
 
-const ProjectCard = ({
-  image,
-  name,
-  address,
-  Hotline,
-  Email,
-  heading,
-  branchPage,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const handleOpenModal = () => setIsOpen(true);
-  const handleCloseModal = () => setIsOpen(false);
+const DoctorSearch = () => {
+  const [displayedDoctors, setDisplayedDoctors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedBranch, setSelectedBranch] = useState("");
+  const [selectedSpecialization, setSelectedSpecialization] = useState("");
+  const [selectedDay, setSelectedDay] = useState("");
+  const [showFemaleDoctors, setShowFemaleDoctors] = useState(false); // State for female doctors
+  const [lastDoctorIndex, setLastDoctorIndex] = useState(12); // Initial number of doctors to display
+  const branches = Array.from(
+    new Set(
+      doctorData1.doctors.flatMap((doc) => doc.chember.map((ch) => ch.branch))
+    )
+  );
+  const specializations = Array.from(
+    new Set(doctorData1.doctors.map((doc) => doc.drSpecilist))
+  );
 
-  const handleAuthorityMapClick = () => {
-    if (branchPage) {
-      // If branchPage exists, show the modal
-      handleOpenModal();
-    } else {
-      // If branchPage doesn't exist, redirect to separate page
-      window.location.href = `/${heading.replace(/\s+/g, "").toLowerCase()}`;
+  useEffect(() => {
+    setDisplayedDoctors(doctorData1.doctors.slice(0, lastDoctorIndex));
+  }, [lastDoctorIndex]);
+
+  useEffect(() => {
+    let result = doctorData1.doctors;
+
+    if (selectedBranch) {
+      result = result.filter((doctor) =>
+        doctor.chember.some((ch) => ch.branch === selectedBranch)
+      );
+    }
+
+    if (selectedSpecialization) {
+      result = result.filter(
+        (doctor) => doctor.drSpecilist === selectedSpecialization
+      );
+    }
+
+    if (selectedDay) {
+      result = result.filter((doctor) =>
+        doctor.chember.some((ch) =>
+          ch.weekday.some((wd) => wd.day === selectedDay)
+        )
+      );
+    }
+
+    if (searchTerm) {
+      result = result.filter((doctor) =>
+        doctor.drName.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+    if (showFemaleDoctors) {
+      result = result.filter((doctor) => doctor.drGender === "Female");
+    }
+
+    setDisplayedDoctors(result.slice(0, lastDoctorIndex));
+  }, [
+    selectedBranch,
+    selectedSpecialization,
+    selectedDay,
+    searchTerm,
+    lastDoctorIndex,
+    showFemaleDoctors,
+  ]);
+
+  // Function to handle lazy loading of doctors
+  const loadMoreDoctors = () => {
+    // Load more doctors if we're at the end of the scroll if there are more doctors to show
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setLastDoctorIndex((prevIndex) => prevIndex + 12);
     }
   };
 
+  // Added event listener for scroll to handle lazy loading
+  useEffect(() => {
+    window.addEventListener("scroll", loadMoreDoctors);
+    return () => window.removeEventListener("scroll", loadMoreDoctors);
+  }, []);
+
   return (
-    <div className="bg-gradient-to-b from-[#F5FFFA] to-[#f0fff0] shadow-2xl rounded-2xl sm:w-[299px] w-full transition-transform duration-700 transform hover:-translate-y-3">
-      <button onClick={handleAuthorityMapClick} className="">
-        <div className="relative w-full">
-          <img
-            src={image}
-            alt="Branch_image"
-            className="w-full shadow-xl rounded-3xl object-cover p-2 "
-          />
+    <div className="bg-[#ffffff]">
+      <div>
+        <div className="flex flex-col pt-[80px] mx-auto max-w-7xl">
+          <h2 className="text-gray-900/50 pb-5 text-center pl-2 text-[28px] font-bold font-ubuntu">
+            CONSULTANTS
+          </h2>
         </div>
-        <div className="px-4 pt-2 pb-3 flex flex-col justify-between">
-          <h1 className="text-[#00984a] px-2 font-ubuntu font-bold text-center text-[25px]">
-            {heading}
-          </h1>
-          <p className="text-gray-600 px-2 font-ubuntu font-semibold text-[16px]">
-            {name}
-          </p>
-          <BranchContact address={address} Hotline={Hotline} Email={Email} />
-          <motion.button
-            className="hover:bg-[#00984a] bg-gray-100 text-[#00984a] hover:text-white hover:font-black font-ubuntu font-medium py-2 px-4 rounded-md mt-2 mx-1 focus:outline-none shadow-md"
+
+        <div className="sticky top-[74px] z-10  rounded-xl shadow-2xl bg-white flex flex-col-reverse gap-2 xl:flex-row p-5 row-span-1 mx-12 xl:mx-auto xl:max-w-7xl justify-between">
+          <motion.input
+            className="px-2 py-1 border text-[#00984a] border-PDCL-green bg-gray-200  rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
+            type="text"
+            placeholder="Search by doctor's name..."
+            layout
+            transition={spring}
+            whileTap={{ scale: 0.9 }}
+            variants={buttonVariants}
+            whileHover="hover"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+
+          <motion.select
+            className="px-2 py-1 border text-[#00984a] border-PDCL-green bg-gray-200 rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
+            onChange={(e) => setSelectedBranch(e.target.value)}
             layout
             transition={spring}
             whileTap={{ scale: 0.9 }}
             variants={buttonVariants}
             whileHover="hover">
-            Branch Details
-          </motion.button>
-          {isOpen && branchPage && (
-            <Details branchPage={branchPage} onClose={handleCloseModal} />
-          )}
+            <option value="">Select Branch</option>
+            {branches.map((branch) => (
+              <option key={branch} value={branch}>
+                {branch}
+              </option>
+            ))}
+          </motion.select>
+
+          <motion.select
+            className="px-2 py-1 border text-[#00984a] border-PDCL-green bg-gray-200 rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
+            onChange={(e) => setSelectedSpecialization(e.target.value)}
+            layout
+            transition={spring}
+            whileTap={{ scale: 0.9 }}
+            variants={buttonVariants}
+            whileHover="hover">
+            <option value="">Select Specialization</option>
+            {specializations.map((spec) => (
+              <option key={spec} value={spec}>
+                {spec}
+              </option>
+            ))}
+          </motion.select>
+
+          <motion.select
+            className="px-2 py-1 border text-[#00984a] border-PDCL-green bg-gray-200 rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
+            onChange={(e) => setSelectedDay(e.target.value)}
+            layout
+            transition={spring}
+            whileTap={{ scale: 0.9 }}
+            variants={buttonVariants}
+            whileHover="hover">
+            <option value="">Select Day</option>
+            {[
+              "Saturday",
+              "Sunday",
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+            ].map((day) => (
+              <option key={day} value={day}>
+                {day}
+              </option>
+            ))}
+          </motion.select>
+          <motion.label
+            className="flex items-center gap-2 px-2 py-1 border text-[#5E2750] border-[#5E2750] bg-[#fce8f3] rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
+            layout
+            transition={spring}
+            whileTap={{ scale: 0.9 }}
+            variants={buttonVariants}
+            whileHover="hover">
+            Female Doctors
+            <motion.input
+              type="checkbox"
+              checked={showFemaleDoctors}
+              onChange={() => setShowFemaleDoctors(!showFemaleDoctors)}
+              className="form-checkbox text-PDCL-green rounded"
+            />
+          </motion.label>
         </div>
-      </button>
-    </div>
-  );
-};
-
-const Branch = () => {
-  const [filterInsideDhaka, setFilterInsideDhaka] = useState(false);
-  const [filterOutsideDhaka, setFilterOutsideDhaka] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [apiData, setApiData] = useState(null); // Store the API data
-
-  // Log API data here
-  useEffect(() => {
-    const fetchBranchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.populardiagnostic.com/api/branches",
-          {
-            params: {
-              token: "UCbuv3xIyFsMS9pycQzIiwdwaiS3izz4",
-            },
-          }
-        );
-        setApiData(response.data); // Store the API data
-        console.log("Fetched data from API:", response.data); // Log the API response
-      } catch (error) {
-        console.error("Error fetching branch data:", error);
-      }
-    };
-
-    fetchBranchData();
-  }, []); // This effect runs only once when the component is mounted
-
-  const insideDhakaProjects = branch.filter(
-    (project) => project.braCity === "Dhaka"
-  );
-  const outsideDhakaProjects = branch.filter(
-    (project) => project.braCity !== "Dhaka"
-  );
-
-  const filteredProjects = branch.filter((project) => {
-    if (filterInsideDhaka && filterOutsideDhaka) {
-      return true; // Show all projects
-    } else if (filterInsideDhaka) {
-      return insideDhakaProjects.includes(project);
-    } else if (filterOutsideDhaka) {
-      return outsideDhakaProjects.includes(project);
-    } else {
-      return true;
-    }
-  });
-
-  const filteredProjectsIncSearch = filteredProjects.filter((project) => {
-    const projectNameLower = project.name.toLowerCase();
-    const searchTermLower = searchTerm.toLowerCase();
-    return projectNameLower.includes(searchTermLower);
-  });
-
-  const handleInsideDhakaToggle = () => {
-    setFilterInsideDhaka(!filterInsideDhaka);
-  };
-
-  const handleOutsideDhakaToggle = () => {
-    setFilterOutsideDhaka(!filterOutsideDhaka);
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
-    <div className="bg-[#ffffff] ">
-      <div className="flex flex-col pt-[80px] mx-auto max-w-7xl">
-        <h2 className="text-gray-900/50 pb-5 text-center pl-2 text-[28px] font-bold font-ubuntu">
-          BRANCHES
-        </h2>
-      </div>
-
-      <div className="sticky top-[70px] z-10 rounded-xl  shadow-2xl bg-white flex flex-col-reverse gap-2 sm:flex-row p-5 row-span-1 mx-12 xl:mx-auto xl:max-w-7xl justify-between">
-        <motion.label
-          className={` ${
-            filterInsideDhaka ? "bg-[#00984a]" : "bg-gray-500"
-          } hover: text-white font-ubuntu font-medium py-2 px-4 rounded-md focus:outline-none shadow-md`}
-          layout
-          transition={spring}
-          whileTap={{ scale: 0.9 }}
-          variants={buttonVariants}
-          whileHover="hover">
-          Inside Dhaka
-          <motion.input
-            type="checkbox"
-            checked={filterInsideDhaka}
-            onChange={handleInsideDhakaToggle}
-            className="ml-2 form-checkbox text-PDCL-green rounded"
-          />
-        </motion.label>
-        <motion.label
-          className={` ${
-            filterOutsideDhaka ? "bg-[#00984a]" : "bg-gray-500"
-          } hover: text-white font-ubuntu font-medium py-2 px-4 rounded-md focus:outline-none shadow-md`}
-          layout
-          transition={spring}
-          whileTap={{ scale: 0.9 }}
-          variants={buttonVariants}
-          whileHover="hover">
-          Outside Dhaka
-          <motion.input
-            type="checkbox"
-            checked={filterOutsideDhaka}
-            onChange={handleOutsideDhakaToggle}
-            className="ml-2 form-checkbox text-PDCL-green rounded"
-          />
-        </motion.label>
-
-        <motion.input
-          className="px-2 py-1 border  text-[#00984a] border-PDCL-green bg-gray-200  rounded-lg focus:outline-none focus:ring-1 focus:ring-PDCL-green"
-          type="text"
-          placeholder="Search Branches"
-          value={searchTerm}
-          onChange={handleSearchChange}
-          variants={searchBoxVariants}
-          whileHover="hover"
-        />
-      </div>
-
-      <div className="flex mx-auto pb-10 pt-[100px] sm:w-[80%] p-3  max-w-7xl justify-center flex-wrap gap-4">
-        {filteredProjectsIncSearch.map((project) => (
-          <ProjectCard key={project.branchID} {...project} />
-        ))}
+        <div className="doctor-list flex mx-auto pb-10 px-3 sm:px-0 pt-[50px] max-w-7xl justify-center flex-wrap gap-5">
+          {displayedDoctors.map((doctor) => (
+            <DoctorCard key={doctor.drID} doctor={doctor} />
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
-export default Branch;
+export default DoctorSearch;
